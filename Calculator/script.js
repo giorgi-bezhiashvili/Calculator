9e0b133c2f2e856a0459ec6805dc2611cb01480c
Giorgi bezhiashvili <giorgibezhiashvili5@gmail.com>
2024-12-29 12:22:39 +0400

    Update script.js
    I added keyboard function

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,8 +1,21 @@
+//variables
 let calculation = localStorage.getItem("eqiation") || ""
 let text = document.getElementById("text")
 let buttons = document.querySelectorAll("#btn");
 let equal = document.getElementById("equal")
+let equalfun = function(){
+    try {
+        calculation= eval(calculation)
+        text.innerText = calculation
+    } catch (error) {
+        text.innerText = "error"
+        calculation = " "
+    }
+    //save to localstorage
+    localStorage.setItem("eqiation" , calculation)
+ }
 
+//get to local storage and if localstoage isn't there get empty string
 text.innerText = localStorage.getItem("eqiation") || " "
 
 
@@ -12,17 +25,28 @@ buttons.forEach((button) => {
         localStorage.setItem("eqiation" , calculation)
 
     });
+
 });
 
+//equal button function
 equal.addEventListener("click" , ()=>{
-    try {
-        calculation= eval(calculation)
-        text.innerText = calculation
-    } catch (error) {
-        text.innerText = "error"
-        calculation = " "
+    equalfun()
+})
+
+//Keyboard
+document.body.addEventListener("keydown" , ()=>{
+    
+    const key = event.key
+    if(event.key === "Enter"){
+        equalfun()
     }
-    localStorage.setItem("eqiation" , calculation)
- 
+    if (event.key === "c") {
+        calculation = ""
+        text.innerText = "Numbers"
+    }else if("1234567890+-*/.".includes(key)){
+        calculation += key
+        text.innerText = calculation
+    }
+    
 })
 
